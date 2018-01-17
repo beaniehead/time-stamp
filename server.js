8035401aad8a51acc08cc19f421efac2a83e4961
Glitch (lumpy-alarm) <none>
2018-01-17 22:04:31 +0000

    👦💶 Checkpoint app.js:576250/1 server.js:576250/44 .env:576250/29 .gitconfig:576250/3 package.json:576250/62 public/style.css:576250/10 routes/index.js:576250/147

--- server.js
+++ server.js
@@ -34,10 +34,9 @@ app.route('/_api/package.json')
   });
   
 app.route('/')
-    .get(function(req, res) {
+    .get((req, res)=> {
 		  res.sendFile(process.cwd() + '/views/index.html');
     })
-
 
 // Respond not found to all the wrong routes
 app.use(function(req, res, next){
